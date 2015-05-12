b8377717f5eb9e27185abcdc610640b07a1b6fcb
Benoît Grélard <benoit.grelard@gmail.com>
2015-05-12 22:42:10 +0100

    Update constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const EVENT_SHOOT_REQUESTED = 'EVENT_SHOOT_REQUESTED';
 export const EVENT_SHOT = 'EVENT_SHOT';
 
-export const CONST_CELL_INIT = 0;
-export const CONST_CELL_MISSED = 1;
+export const CONST_CELL_INIT = 'CONST_CELL_INIT';
+export const CONST_CELL_MISSED = 'CONST_CELL_MISSED';
