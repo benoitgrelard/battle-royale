f463ad3197167c8458fb151328d71e42fa564ee1
Benoît Grélard <benoit.grelard@gmail.com>
2015-05-13 21:52:08 +0100

    [EventEmitter] Return this in every methods

--- src/lib/EventEmitter.js
+++ src/lib/EventEmitter.js
@@ -4,32 +4,35 @@
 export default class EventEmitter {
 
 	constructor () {
-		this._listeners = [];
+		this._$listeners = [];
 	}
 
 	on (eventName, callback) {
-		this._listeners.push({
+		this._$listeners.push({
 			name: eventName,
 			callback: callback
 		});
+		return this;
 	}
 
 	off (eventName, callback) {
-		this._listeners.forEach((listener, index) => {
+		this._$listeners.forEach((listener, index) => {
 			if (listener.name === eventName && listener.callback === callback) {
-				this._listeners.splice(index, 1);
+				this._$listeners.splice(index, 1);
 			}
 		});
+		return this;
 	}
 
 	emit (eventName, data) {
-		this._listeners
+		this._$listeners
 			.filter(listener => listener.name === eventName)
 			.forEach(listener => listener.callback(eventName, data, this));
+		return this;
 	}
 
 	proxy (eventName, data) {
-		this.emit(eventName, data);
+		return this.emit(eventName, data);
 	}
 
 }
